1655decd30944387b6d3e5ab3986ce88f1aa6b21
AJ Miro <miro99@gmail.com>
2018-10-27 14:16:39 -0400

    Create TableColumn class.

---
+++ DbDiff/TableColumn.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbDiff
+{
+    class TableColumn : INamed
+    {
+        string _Name;
+        public string Name
+        {
+            get
+            {
+                return _Name;
+            }
+            private set
+            {
+                _Name = value;
+            }
+        }
+
+        public TableColumn(string name)
+        {
+            Name = name;
+        }
+    }
+}
