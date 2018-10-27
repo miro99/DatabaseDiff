0e8c03ccf15b2937c128ba12e7920cfc951e44d4
AJ Miro <miro99@gmail.com>
2018-10-27 14:34:00 -0400

    Refactored ItemsInDatabase class to use generics. Added InitializeItems method. Added InitializeItemFromReader method

--- DbDiff/ItemsInDatabase.cs
+++ DbDiff/ItemsInDatabase.cs
@@ -1,6 +1,27 @@
-﻿namespace DbDiff
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+
+namespace DbDiff
 {
-    public class ItemsInDatabase
+    public abstract class ItemsInDatabase<T>
     {
+        IEnumerable<T> _AllItems;
+
+        public void InitializeItems(IDataReader dataReader)
+        {
+            List<T> tables = new List<T>();
+            while (dataReader.Read())
+            {
+                T table = InitializeItemFromReader(dataReader);
+                tables.Add(table);
+            }
+            _AllItems = tables;
+        }
+
+        private T InitializeItemFromReader(IDataReader dataReader)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
