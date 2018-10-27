ae1e342df298835322f6f6bb573ed6078a78e114
AJ Miro <miro99@gmail.com>
2018-10-27 15:10:40 -0400

    Removed ListMissingItemNames method from TablesInDatabase class

--- DbDiff/TablesInDatabase.cs
+++ DbDiff/TablesInDatabase.cs
@@ -19,22 +19,6 @@ namespace DbDiff
         {            
         }
 
-        public IEnumerable<INamed> ListMissingItemNames(TablesInDatabase tablesInDB2, Func<IEnumerable<INamed>,IEnumerable<INamed>, IEnumerable<INamed>> diffFunction)
-        {
-            if ((this.AllItems == null) || (tablesInDB2.AllItems == null))
-            {
-                throw new Exception("TablesInDatabase class must be initialized before use");
-            }
-
-            if (this.AllItems.Count() == 0)
-            {
-                throw new Exception("Database of record must have tables defined");
-            }
-
-            IEnumerable<INamed> missingTables = diffFunction(this.AllItems, tablesInDB2.AllItems);
-            return missingTables;
-        }
-
         protected override Table InitializeItemFromReader(IDataReader dataReader)
         {
             Table table = new Table(dataReader.GetString(0));
