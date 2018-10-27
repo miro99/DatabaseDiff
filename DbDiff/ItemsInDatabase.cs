000e376a3a591806065994b2cdb189c4030e1743
AJ Miro <miro99@gmail.com>
2018-10-27 15:06:02 -0400

    Created ListMissingItemNames method

--- DbDiff/ItemsInDatabase.cs
+++ DbDiff/ItemsInDatabase.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Linq;
 
 namespace DbDiff
 {
-    public abstract class ItemsInDatabase<T>
+    public abstract class ItemsInDatabase<T> where T : INamed
     {
         protected IEnumerable<T> _AllItems;
 
@@ -17,6 +18,22 @@ namespace DbDiff
                 tables.Add(table);
             }
             _AllItems = tables;
+        }
+
+        public IEnumerable<T> ListMissingItemNames(ItemsInDatabase<T> itemsInDB2, Func<IEnumerable<T>, IEnumerable<T>, IEnumerable<T>> diffFunction)
+        {
+            if ((this._AllItems == null) || (itemsInDB2._AllItems == null))
+            {
+                throw new Exception("ItemsInDatabase class must be initialized before use");
+            }
+
+            if (this._AllItems.Count() == 0)
+            {
+                throw new Exception("Database of record must have tables defined");
+            }
+
+            IEnumerable<T> missingTables = diffFunction(_AllItems, itemsInDB2._AllItems);
+            return missingTables;
         }
 
         protected abstract T InitializeItemFromReader(IDataReader dataReader);
