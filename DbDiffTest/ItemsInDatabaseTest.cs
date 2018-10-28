cd7072e437d66144d72f138b6b42d2fecc456104
AJ Miro <miro99@gmail.com>
2018-10-28 13:47:32 -0400

    Add ArrangeItemsInDatabase method to ItemsInDatabaseTest

--- DbDiffTest/ItemsInDatabaseTest.cs
+++ DbDiffTest/ItemsInDatabaseTest.cs
@@ -1,4 +1,5 @@
-﻿using Moq;
+﻿using DbDiff;
+using Moq;
 using NUnit.Framework;
 using System.Data;
 using System.Linq;
@@ -8,6 +9,27 @@ namespace DbDiffTest
     [TestFixture]
     class ItemsInDatabaseTest
     {
+        private ItemsInDatabase<MockINamed> ArrangeItemsInDatabase(string itemName)
+        {
+            ItemsInDatabase<MockINamed> mockItemsInDatabase = new MockItemsInDatabase();
+            var mockDataReader = new Mock<IDataReader>(MockBehavior.Strict);
+            bool toggleRead = false;
+            if (itemName.Equals(string.Empty) == true)
+            {
+                toggleRead = false;
+            }
+
+            mockDataReader.Setup(x => x.Read())
+                .Returns(() => toggleRead)
+                .Callback(() => toggleRead = false);
+
+            mockDataReader.Setup(x => x.GetString(0))
+                .Returns(itemName);
+
+            mockItemsInDatabase.InitializeItems(mockDataReader.Object);
+            return mockItemsInDatabase;
+        }
+
         [Test]
         public void InitializeItems_Fills_AllItems()
         {
