1e6b7b148ea8cdfc725c97c0cb280f38198b9b32
AJ Miro <miro99@gmail.com>
2018-10-28 10:46:37 -0400

    Added MockItemsInDatabase class and MockINamed class

---
+++ DbDiffTest/MockItemsInDatabase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DbDiff;
+
+namespace DbDiffTest
+{
+    public class MockItemsInDatabase : ItemsInDatabase<MockINamed>
+    {
+        protected override MockINamed InitializeItemFromReader(IDataReader dataReader)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
